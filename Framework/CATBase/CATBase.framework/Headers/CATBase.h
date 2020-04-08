a5d1c2b2cff3cc8eb12e26bd3af310d3679940a0
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-08 09:10:00 +0800

    CAT

---
+++ Framework/CATBase/CATBase.framework/Headers/CATBase.h
@@ -0,0 +1,20 @@
+//
+//  CATBase.h
+//  CATBase
+//
+//  Created by 王磊 on 2020/4/8.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CATBase.
+FOUNDATION_EXPORT double CATBaseVersionNumber;
+
+//! Project version string for CATBase.
+FOUNDATION_EXPORT const unsigned char CATBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CATBase/PublicHeader.h>
+
+
+#import <CATBase/CATBaseViewController.h>
