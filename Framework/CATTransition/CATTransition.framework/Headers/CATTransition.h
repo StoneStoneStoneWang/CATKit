258c8187ed5fe6fa212ae997b924e1ddf9aae3df
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-13 22:06:17 +0800

    CAT

--- Framework/CATTransition/CATTransition.framework/Headers/CATTransition.h
+++ Framework/CATTransition/CATTransition.framework/Headers/CATTransition.h
@@ -17,3 +17,6 @@ FOUNDATION_EXPORT const unsigned char CATTransitionVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <CATTransition/PublicHeader.h>
 
 
+#import <CATTransition/CATTViewController.h>
+
+#import <CATTransition/CATTNavigationController.h>
