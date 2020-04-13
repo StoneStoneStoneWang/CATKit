b84eea026fc4f895fc2d7dcff1473ada6f62f881
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-13 20:55:54 +0800

    CAT

---
+++ Framework/CATNavi/CATNavi.framework/Headers/CATNavi.h
@@ -0,0 +1,23 @@
+//
+//  CATNavi.h
+//  CATNavi
+//
+//  Created by 王磊 on 2020/4/8.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CATNavi.
+FOUNDATION_EXPORT double CATNaviVersionNumber;
+
+//! Project version string for CATNavi.
+FOUNDATION_EXPORT const unsigned char CATNaviVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CATNavi/PublicHeader.h>
+
+#import <CATNavi/CATNavigationController.h>
+
+#import <CATNavi/UINavigationBar+CAT.h>
+
+#import <CATNavi/UIBarButtonItem+CAT.h>
